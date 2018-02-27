14a8532bb6efe2c288d53dc860ba2188da3f6682
FatimaAU <fatima_kodro@hotmail.com>
2018-02-27 09:27:45 +0100

    Initial commit, random testing

---
+++ DoorControl.Test.Unit/DoorControlUnitsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DoorControl.Test.Unit
+{
+    [TestFixture]
+    public class DoorControlUnitTests
+    {
+        //private xxx _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            //_uut = new xxx();
+        }
+
+        [TestCase(3, 2, 5)]
+        [TestCase(3, -2, 1)]
+        public void Add_AddPosAndNegNumbers_ResultIsCorrect(int a, int b, int result)
+        {
+            Assert.That(a + b, Is.EqualTo(result));
+        }
+    }
+}
