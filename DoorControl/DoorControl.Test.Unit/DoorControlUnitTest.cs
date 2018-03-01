c2e3d812ec7c4b75ac09d1bdd9e0c70415ad024c
Soeren Bech <soeren-bechy7@hotmail.com>
2018-03-01 11:12:20 +0100

    Tilføjede DoorControl

---
+++ DoorControl/DoorControl.Test.Unit/DoorControlUnitTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DoorControl.Test.Unit
+{
+    public class DoorControlUnitTest
+    {
+    }
+}
