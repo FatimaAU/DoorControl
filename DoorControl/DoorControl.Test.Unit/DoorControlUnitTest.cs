c39223c57c57b292991b3acc6a21daff6c85f2c5
FatimaAU <fatima_kodro@hotmail.com>
2018-03-01 11:25:46 +0100

    Random testing

--- DoorControl/DoorControl.Test.Unit/DoorControlUnitTest.cs
+++ DoorControl/DoorControl.Test.Unit/DoorControlUnitTest.cs
@@ -7,7 +7,22 @@ using NUnit.Framework;
 
 namespace DoorControl.Test.Unit
 {
-    public class DoorControlUnitTest
+    [TestFixture]
+    public class DoorControlUnitTests
     {
+        //private xxx _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            //_uut = new xxx();
+        }
+
+        [TestCase(3, 2, 5)]
+        [TestCase(3, -2, 1)]
+        public void Add_AddPosAndNegNumbers_ResultIsCorrect(int a, int b, int result)
+        {
+            Assert.That(a + b, Is.EqualTo(result));
+        }
     }
 }
